55b21be1dd857997c13cb43f39b71de0fb5aad2d
azarashi0002 <41383106+azarashi0002@users.noreply.github.com>
2021-09-30 12:36:00 +0900

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,7 +1,37 @@
-﻿#include<Siv3D.hpp> // OpenSiv3D v0.6.1
+#include<Siv3D.hpp> // OpenSiv3D v0.6.1
 #include<Eigen/Core>
 using namespace Eigen;
 using namespace std;
+
+Eigen::Matrix<Eigen::Vector3d, Eigen::Dynamic, Eigen::Dynamic> ImageToMatrix(const Image& image) {
+	Eigen::Matrix<Eigen::Vector3d, Eigen::Dynamic, Eigen::Dynamic> ret(image.width(), image.height());
+	for (const auto [x, y] : step(image.size())) {
+		const auto [r, g, b, a] = ColorF(image[y][x]);
+		ret(y, x) << r, g, b;
+	}
+	return ret;
+}
+std::array<Eigen::MatrixX3d, 4> PickEdge(const Eigen::Matrix<Eigen::Vector3d, Eigen::Dynamic, Eigen::Dynamic>& panel) {
+	std::array<Eigen::MatrixX3d, 4> ret;
+	std::array<Eigen::Matrix<Eigen::Vector3d, Eigen::Dynamic, 1>, 4> edge = {
+		panel.topRows(1).row(0).transpose().eval(),
+		panel.rightCols(1).col(0).eval(),
+		panel.bottomRows(1).row(0).transpose().reverse().eval(),
+		panel.leftCols(1).col(0).reverse().eval()
+	};
+
+	for (int i : step(4)) {
+		Eigen::MatrixX3d mat(panel.rows(), 3);
+		for (int j : step(panel.rows())) {
+			mat.row(j) = edge[i][j];
+		}
+		ret[i] = mat;
+	}
+	return ret;
+}
+int GetP2Rotate(int p1r, int p1e, int p2e) {
+	return (p1r + p1e + p2e + 2) % 4;
+}
 
 class PPMInfo {
 public:
@@ -29,77 +59,95 @@ public:
 	}cost;
 };
 
+struct Graph {
+	Point p;
+	int r;
+};
+
 void Main()
 {
 	Window::SetStyle(WindowStyle::Sizable);
 
-	constexpr StringView path = U"C:\\Users\\root\\source\\repos\\PLAN_A\\PLAN_A\\testcase\\problem2.ppm";
+	constexpr StringView path = U"testcase/problem2.ppm";
 
-	Image image = PPMDecoder().decode(path);
-	Texture texture(image);
-	PPMInfo info(path);
+	const Image image = PPMDecoder().decode(path);
+	const Texture texture(image);
+	const PPMInfo info(path);
 	const Size tile_size = image.size() / info.divit;
-	Grid<Image> tiles(info.divit, Image(tile_size));
-	for (auto i : step(tiles.size())) {
-		for (auto j : step(tile_size)) {
-			tiles[i][j] = image[i * tile_size + j];
-		}
-	}
-
-	Grid<s3d::Array<MatrixXd>> edge(tiles.size(), s3d::Array<MatrixXd>(4, MatrixXd(tile_size.x, 3)));
-	for (auto i : step(edge.size())) {
-		for (int j : step(tile_size.x)) {
-			edge[i][0](j, 0) = tiles[i][0][j].r;
-			edge[i][0](j, 1) = tiles[i][0][j].g;
-			edge[i][0](j, 2) = tiles[i][0][j].b;
-			edge[i][1](j, 0) = tiles[i][j][tile_size.x - 1].r;
-			edge[i][1](j, 1) = tiles[i][j][tile_size.x - 1].g;
-			edge[i][1](j, 2) = tiles[i][j][tile_size.x - 1].b;
-			edge[i][2](j, 0) = tiles[i][tile_size.y - 1][tile_size.x - 1 - j].r;
-			edge[i][2](j, 1) = tiles[i][tile_size.y - 1][tile_size.x - 1 - j].g;
-			edge[i][2](j, 2) = tiles[i][tile_size.y - 1][tile_size.x - 1 - j].b;
-			edge[i][3](j, 0) = tiles[i][tile_size.y - 1 - j][0].r;
-			edge[i][3](j, 1) = tiles[i][tile_size.y - 1 - j][0].g;
-			edge[i][3](j, 2) = tiles[i][tile_size.y - 1 - j][0].b;
-		}
-	}
+	const Grid<Image> tiles = Grid<Image>::IndexedGenerate(info.divit, [&](const Point& p) {return image.clipped(Rect(p * tile_size, tile_size)); });
+	const auto mat_tiles = (tiles >> [](const auto& image) {return ImageToMatrix(image); });
+	const Grid<std::array<Eigen::MatrixX3d, 4>> edge = (mat_tiles >> [](const auto& mat) {return PickEdge(mat); });
 
 	struct P {
 		Point p1, p2;
 		int r1, r2;
 		double score;
 	};
-	s3d::Array<P> sl;
+	const auto score_calc = [&](const Point& p1, int d1, const Point& p2, int d2) {
+		return (edge[p1][d1] - edge[p2][d2].colwise().reverse()).rowwise().squaredNorm().sum();
+	};
+	s3d::Array<P> scorearray;
 	for (auto i : step(tiles.size())) {
 		for (auto j : step(tiles.size())) {
 			if (i.x < j.x or i.x == j.x and i.y < j.y) {
 				for(auto k : step(4)) {
 					for (int l : step(4)) {
-						double s = 0;
-						for (int m : step(tile_size.x)) {
-							s += (edge[i][k].row(m) - edge[j][l].row(m).reverse()).squaredNorm();
-						}
-						sl << P{ i,j,k,l,s };
+						scorearray << (P{ i,j,k,l,score_calc(i,k,j,l) });
 					}
 				}
 			}
 		}
 	}
-	sl.sort_by([](const auto& a, const auto& b) {return a.score < b.score; });
-	{
+	scorearray.sort_by([](const auto& a, const auto& b) {return a.score < b.score; });
+	list<P> scorelist(scorearray.begin(), scorearray.end());
+
+	{	//textfile output
 		TextWriter writer(U"Banana.txt");
-		for (const auto& i : sl)
+		for (const auto& i : scorelist)
 			writer.writeln(U"{},{},{},{},{}"_fmt(i.p1, i.p2, i.r1, i.r2, i.score));
 	}
-	Image i(image.size());
-	//tiles[sl[0].p1].overwrite(i, { 0,0 });
-	//tiles[sl[1].p2].rotate270().overwrite(i, { tile_size.x,0 });
-	DynamicTexture t(i);
+
+	s3d::Array<shared_ptr<Graph>> nodes;
+	Grid<weak_ptr<Graph>> graph;
+	for (const auto& [p1, p2, d1, d2, score] : scorelist) {
+		auto&& i1 = *find_if(graph.begin(), graph.end(), [&](const shared_ptr<Graph>& node) {return node->p == p1; });
+		auto&& i2 = *find_if(graph.begin(), graph.end(), [&](const shared_ptr<Graph>& node) {return node->p == p2; });
+
+	}
+
+	Image im(image.size());
+	DynamicTexture t(im);
 	while (System::Update())
 	{
-		tiles[sl[0].p1].overwrite(i, { 0,0 });
-		tiles[sl[0].p2].rotated(Math::HalfPi*(int)Scene::Time()).overwrite(i, { tile_size.x,0 });
-		t.fill(i);
+		const auto rotate1 = [](const Image& image, int r) {
+				switch (r) {
+				case 0:
+					return image.rotated180();
+				case 1:
+					return image.rotated90();
+				case 2:
+					return image;
+				case 3:
+					return image.rotated270();
+				}
+			};
+		const auto rotate2 = [](const Image& image, int r) {
+				switch (r) {
+				case 0:
+					return image;
+				case 1:
+					return image.rotated270();
+				case 2:
+					return image.rotated180();
+				case 3:
+					return image.rotated90();
+				}
+			};
+		for (auto i : step(Size(16, 5))) {
+			rotate1(tiles[scorearray[i.x + i.y * 16].p1], scorearray[i.x + i.y * 16].r1).overwrite(im, { tile_size.x * i.x,tile_size.y * i.y * 2 });
+			rotate2(tiles[scorearray[i.x + i.y * 16].p2], scorearray[i.x + i.y * 16].r2).overwrite(im, { tile_size.x * i.x,tile_size.y * (i.y * 2 + 1) });
+		}
+		t.fill(im);
 		t.draw();
 	}
 }
